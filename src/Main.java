e81b4b4daf12bb0efe733e21fe3aebb0adc9c02b
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-10 10:06:48 -0500

    Finished selection functionality of main method, as well as save contacts method

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +15,61 @@ public class Main {
         ContactList contacts = new ContactList(filePath);
 
         Scanner scanner = new Scanner(System.in);
+
+        label:
+        while (true) {
+            if (contacts.getContactList().isEmpty()) {
+                while (true) {
+                    printDefault();
+                    String input = scanner.nextLine();
+                    if (input.equals("1")) {
+                        contacts.createNewContact();
+                        break;
+                    } else if (input.equals("2")) {
+                        System.out.println("Exiting Contacts Manager.");
+                        break label;
+                    } else {
+                        System.out.println("That is not a valid option. Please try again.");
+                        System.out.println("Press enter to continue.");
+                        scanner.nextLine();
+                    }
+                }
+            } else {
+                label1:
+                while (true) {
+                    printMenu();
+                    String input = scanner.nextLine();
+
+                    switch (input) {
+                        case "1":
+                            contacts.printContacts();
+                            System.out.println("Press enter to continue.");
+                            scanner.nextLine();
+                            break;
+                        case "2":
+                            contacts.createNewContact();
+                            break;
+                        case "3":
+                            contacts.searchContacts();
+                            break;
+                        case "4":
+                            contacts.deleteContact();
+                            break label1;
+                        case "5":
+                            System.out.println("Saving your contacts.");
+                            List<String> stringList = contacts.makeStringList();
+                            saveContacts(filePath, stringList);
+                            System.out.println("Exiting Contacts Manager.");
+                            break label;
+                        default:
+                            System.out.println("That is not a valid option. Please try again.");
+                            System.out.println("Press enter to continue.");
+                            scanner.nextLine();
+                            break;
+                    }
+                }
+            }
+        }
     }
 
     public static Path createDirectoryAndFile(String dirName, String fileName) throws IOException {
@@ -47,4 +103,8 @@ public class Main {
 		System.out.println("4. Delete an existing contact.");
 		System.out.println("5. Exit.");
 	}
+
+    public static void saveContacts(Path filepath, List<String> stringList) throws IOException {
+        Files.write(filepath, stringList);
+    }
 }

fea40784f439443c7c41cd40877d2fa985eac34e
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-10 10:29:13 -0500

    Beginning of search by first/last/number functionality

--- src/ContactList.java
+++ src/ContactList.java
@@ -12,9 +12,10 @@ public class ContactList {
 			while (scanner.hasNextLine()) {
 				String row = scanner.nextLine();
 				String[] parts = row.split(",");
-				String name = parts[0];
-				String number = parts[1];
-				this.contactList.add(new Contact(name, number));
+				String firstName = parts[0];
+				String lastName = parts[1];
+				String number = parts[2];
+				this.contactList.add(new Contact(firstName, lastName, number));
 			}
 		} catch (Exception e) {
 			System.out.println("Error: " + e.getMessage());
@@ -29,7 +30,7 @@ public class ContactList {
 		System.out.printf("%-15s | %-15s%n", "Name", "Phone");
 		System.out.println("---------------------------------");
 		for (Contact contact : this.contactList) {
-			System.out.printf("%-15s | %-15s%n", contact.getName(), contact.getNumber());
+			System.out.printf("%-15s %s | %-15s%n", contact.getFirstName(), contact.getLastName(), contact.getNumber());
 		}
 		System.out.println("---------------------------------");
 		System.out.println();
@@ -39,17 +40,19 @@ public class ContactList {
 		Scanner scanner = new Scanner(System.in);
 		label:
 		while (true) {
-			System.out.println("What is their name?");
-			String newName = scanner.nextLine();
+			System.out.println("What is their first name?");
+			String newFirstName = scanner.nextLine();
+			System.out.println("What is their last name?");
+			String newLastName = scanner.nextLine();
 			System.out.println("What is their number?");
 			String newNumber = scanner.nextLine();
 			while (true) {
-				System.out.printf("New contact: %s, %s%n", newName, newNumber);
+				System.out.printf("New contact: %s %s, %s%n", newFirstName, newLastName, newNumber);
 				System.out.println("1. Confirm adding new contact.");
 				System.out.println("2. Cancel adding new contact.");
 				String confirm = scanner.nextLine();
 				if (confirm.equals("1")) {
-					this.contactList.add(new Contact(newName, newNumber));
+					this.contactList.add(new Contact(newFirstName, newLastName, newNumber));
 					System.out.println("New Contact added!");
 					System.out.println("Press enter to continue.");
 					scanner.nextLine();
@@ -68,6 +71,68 @@ public class ContactList {
 		}
 	}
 
+	public void searchSelection() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("How would you like to search?");
+		System.out.println("1. Search by first name.");
+		System.out.println("2. Search by last name.");
+		System.out.println("3. Search by phone number.");
+		System.out.println("4. Exit search menu.");
+		String selection = scanner.nextLine();
+
+
+		label:
+		while (true) {
+			switch (selection) {
+				case "1":
+					//firstNameSearch();
+					break label;
+				case "2":
+					//lastNameSearch();
+					break label;
+				case "3":
+					//phoneNumberSearch();
+					break label;
+				case "4":
+					//break;
+					break label;
+				default:
+					System.out.println("That is not a valid option. Please try again.");
+					System.out.println("Press enter to continue.");
+					scanner.nextLine();
+					break;
+			}
+		}
+
+	}
+
+	public void firstNameSearch() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter the first name to search.");
+		String nameToSearch = scanner.nextLine();
+		int numberOfContacts = 0;
+		ArrayList<Contact> foundContacts = new ArrayList<>();
+		for (Contact contact : this.contactList) {
+			if (contact.getFirstName().equalsIgnoreCase(nameToSearch)) {
+				numberOfContacts++;
+				foundContacts.add(contact);
+			}
+		}
+		if (numberOfContacts == 0) {
+			System.out.println("No contacts found with that name. Please try again.");
+		} else {
+			System.out.printf("%d contacts found.%n", numberOfContacts);
+			for (Contact contact : foundContacts) {
+				System.out.println("Name: " + contact.getFullName());
+				System.out.println("Number: " + contact.getNumber());
+				System.out.println("---");
+			}
+		}
+		System.out.println("Press enter to continue.");
+		scanner.nextLine();
+	}
+
+	/*
 	public void searchContacts() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please enter the name to search.");
@@ -94,6 +159,9 @@ public class ContactList {
 		scanner.nextLine();
 	}
 
+
+	 */
+
 	public void deleteContact() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please enter the name of the contact to delete:");
@@ -101,7 +169,7 @@ public class ContactList {
 		int numberOfContacts = 0;
 		ArrayList<Contact> foundContacts = new ArrayList<>();
 		for (Contact contact : this.contactList) {
-			if (contact.getName().equalsIgnoreCase(nameToDelete)) {
+			if (contact.getFullName().equalsIgnoreCase(nameToDelete)) {
 				numberOfContacts++;
 				foundContacts.add(contact);
 			}
@@ -114,7 +182,7 @@ public class ContactList {
 				System.out.printf("%d contacts were found with that name.%n", numberOfContacts);
 				System.out.println("Please select which contact you wish to delete:");
 				for (int i = 0; i < foundContacts.size(); i++) {
-					System.out.println(i + ". Name: " + foundContacts.get(i).getName() + ", Number: " + foundContacts.get(i).getNumber());
+					System.out.println(i + ". Name: " + foundContacts.get(i).getFullName() + ", Number: " + foundContacts.get(i).getNumber());
 					System.out.println("---");
 				}
 				while (true) {
@@ -126,7 +194,7 @@ public class ContactList {
 							scanner.nextLine();
 						} else {
 							System.out.println("You have selected this contact: ");
-							System.out.println(selectDelete + ". Name: " + foundContacts.get(selectDelete).getName() + ", Number: " + foundContacts.get(selectDelete).getNumber());
+							System.out.println(selectDelete + ". Name: " + foundContacts.get(selectDelete).getFullName() + ", Number: " + foundContacts.get(selectDelete).getNumber());
 							System.out.println("Are you sure you wish to delete this contact?");
 							System.out.println("1. Confirm delete.");
 							System.out.println("2. Cancel delete.");
@@ -163,9 +231,10 @@ public class ContactList {
 	public List<String> makeStringList() {
 		List<String> stringList = new ArrayList<>();
 		for (Contact contact : this.contactList) {
-			String name = contact.getName();
+			String firstName = contact.getFirstName();
+			String lastName = contact.getLastName();
 			String number = contact.getNumber();
-			stringList.add(name + "," + number);
+			stringList.add(firstName + "," + lastName + "," + number);
 		}
 		return stringList;
 	}
